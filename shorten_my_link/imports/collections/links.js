dfa3c9300165644d0109f29c28f953cc6690d30b
Thilo Roth <thilo@thilo.io>
2016-11-06 16:30:07 +0100

    link shortener

---
+++ shorten_my_link/imports/collections/links.js
@@ -0,0 +1,16 @@
+import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check';
+
+Meteor.methods({
+    'links.insert': function(url) {
+        // validate the url
+        check(url, Match.Where(url => validUrl.isUri(url)));
+
+        // save the url
+        const token = Math.random().toString(36).slice(-5);
+        Links.insert({ url, token, clicks: 0 });
+    }
+});
+
+export const Links = new Mongo.Collection('links');
---
+++ shorten_my_link/server/main.js
@@ -0,0 +1,30 @@
+import { Meteor } from 'meteor/meteor';
+import { Links } from '../imports/collections/links';
+import { WebApp } from 'meteor/webapp';
+import ConnectRoute from 'connect-route';
+
+Meteor.startup(() => {
+    Meteor.publish('links', function() {
+        return Links.find({});
+    });
+});
+
+// Executed whenever a user visits with a route like http://localhost:3000/abcd
+function onRoute(req, res, next) {
+    const link = Links.findOne({ token: req.params.token });
+
+    if(link) {
+        Links.update(link, { $inc: { clicks: 1 }});
+        res.writeHead(307, { 'Location': link.url });
+        res.end();
+    } else {
+        next();
+    }
+
+}
+
+const middleware = ConnectRoute(function(router) {
+    router.get('/:token', onRoute);
+});
+
+WebApp.connectHandlers.use(middleware);
